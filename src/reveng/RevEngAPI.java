6a968b0790addd178a18535beb9c222aa3c13930
IanDarwin <>
2011-09-22 15:01:04 +0000

    Imports police

--- src/reveng/RevEngAPI.java
+++ src/reveng/RevEngAPI.java
@@ -2,7 +2,6 @@ package reveng;
 
 import java.io.File;
 import java.io.FileWriter;
-import java.io.IOException;
 import java.io.PrintWriter;
 import java.lang.reflect.Constructor;
 import java.lang.reflect.Field;
